631c3c6f88a8a041bd89ee6eae8485c9128959aa
sebastiii <sebastiii@team-mediaportal.com>
2014-04-21 10:14:00 +0200

    Revert "Merge remote-tracking branch 'remotes/upstream/MP1-4420-BUG-Startup_resume_not_used_correctly_fix_multiple_POWERBROADCAST' into 1.7.0Fork-BDJ"
    This reverts commit 8141e7fd259490ad037b33531ba84cfd164d1c3b, reversing
    changes made to da8259a2035720ab8026e7b4df6d869599f2b025.

--- TvEngine3/TVLibrary/TvPlugin/TvPlugin/TVHome.cs
+++ TvEngine3/TVLibrary/TvPlugin/TvPlugin/TVHome.cs
@@ -120,6 +120,8 @@ namespace TvPlugin
     private static bool _preferAudioTypeOverLang = false;
     private static bool _autoFullScreen = false;
     private static bool _suspended = false;
+    private static bool _onSuspended = false;
+    private static bool _resumed = false;
     private static bool _showlastactivemodule = false;
     private static bool _showlastactivemoduleFullscreen = false;
     private static bool _playbackStopped = false;
@@ -160,6 +162,9 @@ namespace TvPlugin
     [SkinControl(14)]
     protected GUIButtonControl btnActiveRecordings = null;
 
+    private static List<Message> _listThreadMessages = new List<Message>();
+    private static readonly object _listThreadMessagesLock = new object();
+
     // error handling
     public class ChannelErrorInfo
     {
@@ -195,12 +200,14 @@ namespace TvPlugin
 
     private static event OnChannelChangedDelegate OnChannelChanged;
     private delegate void OnChannelChangedDelegate();
+    private static event ThreadMessageHandler OnThreadMessageHandler;
 
     #endregion
 
     #region delegates
 
     private delegate void StopPlayerMainThreadDelegate();
+    private delegate void ThreadMessageHandler(object sender, Message message);
 
     #endregion
 
@@ -1595,6 +1602,44 @@ namespace TvPlugin
       }
     }
 
+    private void DispatchThreadMessages()
+    {
+      if (_listThreadMessages.Count > 0)
+      {
+        List<Message> list;
+        lock (_listThreadMessagesLock) // need lock when switching queues
+        {
+          list = _listThreadMessages;
+          _listThreadMessages = new List<Message>();
+        }
+        for (int i = 0; i < list.Count; ++i)
+        {
+          Message message = list[i];
+          WndProc(ref message);
+        }
+      }
+    }
+
+    /// <summary>
+    /// send thread message. Same as sendmessage() however message is placed on a queue
+    /// which is processed later.
+    /// </summary>
+    /// <param name="message">new message to send</param>
+    private static void SendThreadMessage(ref Message message)
+    {
+      if (OnThreadMessageHandler != null)
+      {
+        OnThreadMessageHandler(null, message);
+      }
+      if (message != null)
+      {
+        lock (_listThreadMessagesLock)
+        {
+          _listThreadMessages.Add(message);
+        }
+      }
+    }
+
     #endregion
 
     public static void OnSelectGroup()
@@ -1671,7 +1716,14 @@ namespace TvPlugin
 
     private void OnSuspend()
     {
-      Log.Debug("TVHome.OnSuspend()");
+      Log.Info("TVHome.OnSuspend()");
+      // OnSuspend already in progress
+      if (_suspended)
+      {
+        Log.Info("TVHome: Suspend is already in progress");
+        _onSuspended = false;
+        return;
+      }
 
       RemoteControl.OnRemotingDisconnected -=
         new RemoteControl.RemotingDisconnectedDelegate(RemoteControl_OnRemotingDisconnected);
@@ -1694,12 +1746,20 @@ namespace TvPlugin
       {
         _ServerNotConnectedHandled = false;
         _suspended = true;
+        _onSuspended = false;
+        _resumed = false;
+        DispatchThreadMessages();
       }
     }
 
     private void OnResume()
     {
-      Log.Debug("TVHome.OnResume()");
+      Log.Info("TVHome.OnResume()");
+      if (_resumed)
+      {
+        Log.Info("TVHome: Resuming is already in progress");
+        return;
+      }
       try
       {
         Connected = false;
@@ -1720,6 +1780,7 @@ namespace TvPlugin
       finally
       {
         _suspended = false;
+        _resumed = true;
       }
     }
 
@@ -1737,28 +1798,44 @@ namespace TvPlugin
     {
       if (msg.Msg == WM_POWERBROADCAST)
       {
+        Log.Warn("TVHome.WndProc()");
         switch (msg.WParam.ToInt32())
         {
           case PBT_APMSTANDBY:
             Log.Info("TVHome.WndProc(): Windows is going to standby");
+            _onSuspended = true;
             OnSuspend();
             break;
           case PBT_APMSUSPEND:
             Log.Info("TVHome.WndProc(): Windows is suspending");
+            _onSuspended = true;
             OnSuspend();
             break;
           case PBT_APMQUERYSUSPEND:
           case PBT_APMQUERYSTANDBY:
             Log.Info("TVHome.WndProc(): Windows is going into powerstate (hibernation/standby)");
-
             break;
           case PBT_APMRESUMESUSPEND:
             Log.Info("TVHome.WndProc(): Windows has resumed from hibernate mode");
-            OnResume();
+            if (_onSuspended)
+            {
+              SendThreadMessage(ref msg);
+            }
+            else
+            {
+              OnResume();
+            }
             break;
           case PBT_APMRESUMESTANDBY:
             Log.Info("TVHome.WndProc(): Windows has resumed from standby mode");
-            OnResume();
+            if (_onSuspended)
+            {
+              SendThreadMessage(ref msg);
+            }
+            else
+            {
+              OnResume();
+            }
             break;
         }
       }

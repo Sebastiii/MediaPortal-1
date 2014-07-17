f8bfb0106d5917a4e978a16c93544223333c4493
michael--t <michael_t@team-mediaportal.com>
2014-07-17 17:30:05 +0200

    MP1-4537 : Modified log4net.config files not to append to existing files on startup

--- Common-MP-TVE3/Common.Utils/Logger/CommonLog4NetLogger.cs
+++ Common-MP-TVE3/Common.Utils/Logger/CommonLog4NetLogger.cs
@@ -69,7 +69,7 @@ namespace MediaPortal.Common.Utils.Logger
       tw.WriteLine("    <appender name=\"DefaultLogAppender\" type=\"log4net.Appender.RollingFileAppender\">");
       tw.WriteLine("      <file value=\"[Name].log\" />");
       tw.WriteLine("      <encoding type=\"System.Text.UTF8Encoding\" />");
-      tw.WriteLine("      <appendToFile value=\"true\" />");
+      tw.WriteLine("      <appendToFile value=\"false\" />");
       tw.WriteLine("      <lockingModel type=\"log4net.Appender.FileAppender+MinimalLock\" />");
       tw.WriteLine("      <rollingStyle value=\"Size\" />");
       tw.WriteLine("      <maxSizeRollBackups value=\"5\" />");
@@ -84,7 +84,7 @@ namespace MediaPortal.Common.Utils.Logger
       tw.WriteLine("    <appender name=\"ErrorLogAppender\" type=\"log4net.Appender.RollingFileAppender\">");
       tw.WriteLine("      <file value=\"[Name]-Error.log\" />");
       tw.WriteLine("      <encoding type=\"System.Text.UTF8Encoding\" />");
-      tw.WriteLine("      <appendToFile value=\"true\" />");
+      tw.WriteLine("      <appendToFile value=\"false\" />");
       tw.WriteLine("      <lockingModel type=\"log4net.Appender.FileAppender+MinimalLock\" />");
       tw.WriteLine("      <rollingStyle value=\"Size\" />");
       tw.WriteLine("      <maxSizeRollBackups value=\"5\" />");

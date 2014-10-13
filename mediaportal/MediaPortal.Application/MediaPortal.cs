a11ae6ead8f71d302ccf7a0a2758610cc6c1fb02
sebastiii <sebastiii@team-mediaportal.com>
2014-10-13 13:30:56 +0200

    MP1-4578: Fix MP freeze on resume related to wrong window handle detection (MP not focused)

--- mediaportal/MediaPortal.Application/MediaPortal.cs
+++ mediaportal/MediaPortal.Application/MediaPortal.cs
@@ -127,9 +127,11 @@ public class MediaPortalApp : D3D, IRender
   private IntPtr                _displayStatusHandle;
   private IntPtr                _userPresenceHandle;
   private IntPtr                _awayModeHandle;
+  private IntPtr                _hWnd;
   private bool                  _resumedAutomatic;
   private bool                  _userActivity;
   private Timer                 _delayTimer;
+  private FormWindowState       _previousWindowState;
 
   // ReSharper disable InconsistentNaming
   private const int WM_SYSCOMMAND            = 0x0112; // http://msdn.microsoft.com/en-us/library/windows/desktop/ms646360(v=vs.85).aspx
@@ -308,6 +310,67 @@ public class MediaPortalApp : D3D, IRender
   // ReSharper restore InconsistentNaming
   // ReSharper restore UnusedMember.Local
 
+  /// <summary>Enumeration of the different ways of showing a window using 
+  /// ShowWindow</summary>
+  private enum WindowShowStyle : uint
+  {
+    /// <summary>Hides the window and activates another window.</summary>
+    /// <remarks>See SW_HIDE</remarks>
+    Hide = 0,
+    /// <summary>Activates and displays a window. If the window is minimized 
+    /// or maximized, the system restores it to its original size and 
+    /// position. An application should specify this flag when displaying 
+    /// the window for the first time.</summary>
+    /// <remarks>See SW_SHOWNORMAL</remarks>
+    ShowNormal = 1,
+    /// <summary>Activates the window and displays it as a minimized window.</summary>
+    /// <remarks>See SW_SHOWMINIMIZED</remarks>
+    ShowMinimized = 2,
+    /// <summary>Activates the window and displays it as a maximized window.</summary>
+    /// <remarks>See SW_SHOWMAXIMIZED</remarks>
+    ShowMaximized = 3,
+    /// <summary>Maximizes the specified window.</summary>
+    /// <remarks>See SW_MAXIMIZE</remarks>
+    Maximize = 3,
+    /// <summary>Displays a window in its most recent size and position. 
+    /// This value is similar to "ShowNormal", except the window is not 
+    /// actived.</summary>
+    /// <remarks>See SW_SHOWNOACTIVATE</remarks>
+    ShowNormalNoActivate = 4,
+    /// <summary>Activates the window and displays it in its current size 
+    /// and position.</summary>
+    /// <remarks>See SW_SHOW</remarks>
+    Show = 5,
+    /// <summary>Minimizes the specified window and activates the next 
+    /// top-level window in the Z order.</summary>
+    /// <remarks>See SW_MINIMIZE</remarks>
+    Minimize = 6,
+    /// <summary>Displays the window as a minimized window. This value is 
+    /// similar to "ShowMinimized", except the window is not activated.</summary>
+    /// <remarks>See SW_SHOWMINNOACTIVE</remarks>
+    ShowMinNoActivate = 7,
+    /// <summary>Displays the window in its current size and position. This 
+    /// value is similar to "Show", except the window is not activated.</summary>
+    /// <remarks>See SW_SHOWNA</remarks>
+    ShowNoActivate = 8,
+    /// <summary>Activates and displays the window. If the window is 
+    /// minimized or maximized, the system restores it to its original size 
+    /// and position. An application should specify this flag when restoring 
+    /// a minimized window.</summary>
+    /// <remarks>See SW_RESTORE</remarks>
+    Restore = 9,
+    /// <summary>Sets the show state based on the SW_ value specified in the 
+    /// STARTUPINFO structure passed to the CreateProcess function by the 
+    /// program that started the application.</summary>
+    /// <remarks>See SW_SHOWDEFAULT</remarks>
+    ShowDefault = 10,
+    /// <summary>Windows 2000/XP: Minimizes a window, even if the thread 
+    /// that owns the window is hung. This flag should only be used when 
+    /// minimizing windows from a different thread.</summary>
+    /// <remarks>See SW_FORCEMINIMIZE</remarks>
+    ForceMinimized = 11
+  }
+
   #endregion
 
   #region structs
@@ -429,6 +492,16 @@ public class MediaPortalApp : D3D, IRender
 
   [DllImport("user32.dll", SetLastError = true)]
   static extern bool PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+  [DllImport("user32.dll", SetLastError = true)]
+  public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+  [DllImport("user32.dll", SetLastError = true)]
+  public static extern void SwitchToThisWindow(IntPtr hWnd, bool fAltTab);
+
+  [DllImport("user32.dll", SetLastError = true)]
+  [return: MarshalAs(UnmanagedType.Bool)]
+  private static extern bool ShowWindow(IntPtr hWnd, WindowShowStyle nCmdShow);
 
   #endregion
 
@@ -1638,6 +1711,17 @@ public class MediaPortalApp : D3D, IRender
       {
         // The computer is about to enter a suspended state
         case (int)PBT_EVENT.PBT_APMSUSPEND:
+          // Save current MediaPortal Windows handle and check WindowsState
+          _previousWindowState = WindowState;
+          RestoreFromTray();
+          Process prc = Process.GetCurrentProcess();
+          Process.GetProcessesByName(prc.ProcessName);
+          Log.Debug("Main: MediaPortal WindowsHandle {0}", prc.MainWindowHandle);
+          if (prc.ProcessName == "MediaPortal")
+          {
+            _hWnd = prc.MainWindowHandle;
+          }
+
           // Reset timer and resume states
           if (_delayTimer != null)
           {
@@ -1818,10 +1902,9 @@ public class MediaPortalApp : D3D, IRender
 
     // Send PBT_APMRESUMEDELAYED message
     Log.Debug("Main: SendResumeDelayedMsg - sending PBT_APMRESUMEDELAYED message");
-    IntPtr hWnd = Form.ActiveForm.Handle;
-    if (hWnd != IntPtr.Zero)
+    if (_hWnd != IntPtr.Zero)
     {
-      PostMessage(hWnd, WM_POWERBROADCAST, new IntPtr((int)PBT_EVENT.PBT_APMRESUMEDELAYED), IntPtr.Zero);
+      PostMessage(_hWnd, WM_POWERBROADCAST, new IntPtr((int)PBT_EVENT.PBT_APMRESUMEDELAYED), IntPtr.Zero);
     }
   }
 
@@ -2548,6 +2631,22 @@ public class MediaPortalApp : D3D, IRender
 
     _suspended = false;
     _lastOnresume = DateTime.Now;
+
+    // Force Focus after resume done (really weird sequence)
+    if (_previousWindowState != FormWindowState.Minimized)
+    {
+      SwitchToThisWindow(_hWnd, true);
+      ShowWindow(_hWnd, WindowShowStyle.Show);
+      ShowWindow(_hWnd, WindowShowStyle.Minimize);
+      ShowWindow(_hWnd, WindowShowStyle.Restore);
+      ShowWindow(_hWnd, WindowShowStyle.ShowNormal);
+      SetForegroundWindow(_hWnd);
+    }
+    else
+    {
+      MinimizeToTray();
+    }
+
     Log.Info("Main: OnResumeSuspend - Done");
   }
 
